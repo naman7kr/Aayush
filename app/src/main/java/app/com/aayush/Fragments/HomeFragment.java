56228c91496c219766121de8a483f8ae354465de
KumarNaman <naman7kr@gmail.com>
2017-11-27 14:46:05 +0530

    WK-MainPageCard basic

--- app/src/main/java/app/com/aayush/Fragments/HomeFragment.java
+++ app/src/main/java/app/com/aayush/Fragments/HomeFragment.java
@@ -23,71 +23,51 @@ public class HomeFragment extends BaseFragment implements TabLayout.OnTabSelecte
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private View view;
-    private LinearLayout linearLayout;
-    private ImageView cardType;
-    private ImageView cardselectToggle;
+    private static LinearLayout linearLayout;
+    static ImageView cardType;
+    static ImageView cardselectToggle;
+    private MainActivity mainActivity;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_home,container,false);
         this.view=view;
+        mainActivity= (MainActivity) getActivity();
         tabLayout=view.findViewById(R.id.card_tablayout);
         viewPager=view.findViewById(R.id.card_viewpager);
         CardAdapter adapter=new CardAdapter(getChildFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
-        tabLayout.addOnTabSelectedListener(this);
-        linearLayout = (LinearLayout)
-                LayoutInflater.from(getActivity()).inflate(R.layout.card_tabs, tabLayout, false);
-        cardType=linearLayout.findViewById(R.id.card_typeimg);
-        cardselectToggle=linearLayout.findViewById(R.id.card_togglebutton);
-         for (int i = 0; i < tabLayout.getTabCount(); i++) {
-            TabLayout.Tab tab = tabLayout.getTabAt(i);
-             linearLayout = (LinearLayout)
-                     LayoutInflater.from(getActivity()).inflate(R.layout.card_tabs, tabLayout, false);
-             cardType=linearLayout.findViewById(R.id.card_typeimg);
-             cardselectToggle=linearLayout.findViewById(R.id.card_togglebutton);
-             if(tab.isSelected())
-             {
 
-                 cardselectToggle.setImageResource(R.mipmap.ic_toggle_button_on);
-             }
-             else
-             {
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            linearLayout = (LinearLayout)
+                    LayoutInflater.from(mainActivity).inflate(R.layout.card_tabs, tabLayout, false);
+            cardType=linearLayout.findViewById(R.id.card_typeimg);
+            cardselectToggle=linearLayout.findViewById(R.id.card_togglebutton);
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if(tab.isSelected())
+                cardselectToggle.setImageResource(R.mipmap.ic_toggle_button_on);
+            else
                 cardselectToggle.setImageResource(R.mipmap.ic_toggle_button_off);
-             }
-             cardType.setImageResource(R.mipmap.ic_launcher);
+            Log.e("lol","wtf");
+            cardType.setImageResource(R.mipmap.ic_launcher);
             tab.setCustomView(linearLayout);
         }
-
+        tabLayout.addOnTabSelectedListener(this);
         return view;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-    }
-
-    @Override
     public void onTabSelected(TabLayout.Tab tab) {
-        linearLayout = (LinearLayout)
-                LayoutInflater.from(getActivity()).inflate(R.layout.card_tabs, tabLayout, false);
-        cardselectToggle=linearLayout.findViewById(R.id.card_togglebutton);
-        cardselectToggle.setImageResource(R.mipmap.ic_toggle_button_on);
-        cardType.setImageResource(R.mipmap.ic_launcher);
-           tab.setCustomView(linearLayout);
-
+       cardselectToggle=tab.getCustomView().findViewById(R.id.card_togglebutton);
+       cardselectToggle.setImageResource(R.mipmap.ic_toggle_button_on);
     }
 
     @Override
     public void onTabUnselected(TabLayout.Tab tab) {
-        linearLayout = (LinearLayout)
-                LayoutInflater.from(getActivity()).inflate(R.layout.card_tabs, tabLayout, false);
-        cardselectToggle=linearLayout.findViewById(R.id.card_togglebutton);
+        cardselectToggle=tab.getCustomView().findViewById(R.id.card_togglebutton);
         cardselectToggle.setImageResource(R.mipmap.ic_toggle_button_off);
-        cardType.setImageResource(R.mipmap.ic_launcher);
-        tab.setCustomView(linearLayout);
     }
 
     @Override
