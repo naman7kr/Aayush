0e24bca4d0cbd1a99f783efa9a3040fc66ce30d8
KumarNaman <naman7kr@gmail.com>
2018-01-31 22:13:10 +0530

    wkn-new Model Categroies added

---
+++ app/src/main/java/app/com/aayush/Modals/Categories.java
@@ -0,0 +1,27 @@
+package app.com.aayush.Modals;
+
+
+
+
+/**
+ * Created by Stan on 1/26/2018.
+ */
+
+public class Categories  {
+
+    private String title;
+
+    public Categories(String title) {
+        this.title=title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+}
